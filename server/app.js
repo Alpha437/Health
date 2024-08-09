e9f17b4136fb6c626e933c5a4ac730de08f64e0f
Alpha437 <55028028+Alpha437@users.noreply.github.com>
2024-08-09 08:18:40 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //optional
 
-app.get('/ping', (req, res) => {
+app.get('/', (req, res) => {
   return res.send({
     error: false,
     message: 'Server is healthy',
